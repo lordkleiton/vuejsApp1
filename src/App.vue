34c9cfbddec7fa17a223af23e7173ed4db246353
mario <m_s_r@outlook.com>
2019-03-26 11:23:19 -0300

    transformando numa versão modularizada do teste1.html

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,30 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <lista @contador="editaTexto" :array="'oie'"></lista>
+    <p>{{ recebido }}</p>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import lista from './components/lista.vue'
 
 export default {
   name: 'app',
+  data: () => {
+      return {
+        recebido: undefined
+      }
+  },
   components: {
-    HelloWorld
+    HelloWorld,
+    lista
+  },
+  methods: {
+    editaTexto(e){
+        this.recebido = 'o ultimo numero recebido dos filhos foi: ' + e;
+    }
   }
 }
 </script>
---
+++ src/components/lista.vue
@@ -0,0 +1,36 @@
+<template>
+    <ul class="lista">
+        <li 
+            :title="'n-não clique em mim por favor'" 
+            @mouseover="aumenta()" 
+            @click="alerta('b-baka!!')" 
+            v-for="(item, index, key) in array"
+            :key="key">
+            {{ key }} Esse é meu index: {{ index }} e esse é meu conteúdo: {{ item }}, mas você passou o mouse em cima {{ contador }} vezes
+        </li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        name: 'lista',
+        props: ['array'],
+        data: function(){
+                return {
+                    contador: 0
+                };
+            },
+        methods:{
+            alerta(msg){
+                alert(msg);
+            },
+            log(msg){
+                console.log(msg);
+            },
+            aumenta(){
+                this.log(`mouse passou em cima ${++this.contador} vezes`);
+                this.$emit('contador', this.contador);
+            }
+        }
+    }
+</script>
